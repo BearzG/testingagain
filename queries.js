be62a24260f1d4c72da0c133cdaad4d47d19ab63
Elmer <elmercrag@gmail.com>
2021-06-02 10:26:36 -0500

    Database created

---
+++ queries.js
@@ -0,0 +1,17 @@
+"use strict"
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "Elmercanelo123"
+});
+
+var element = "Elmer";
+var element = "Hachy";
+
+console.log(element);
+
+username = "jejeje"
+
+connection.query();
